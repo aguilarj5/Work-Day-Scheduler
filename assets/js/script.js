059a5d3a7a05e670c5d4ec58e1492b76477b6969
Jags <jon.kmjt@gmail.com>
2022-12-29 15:01:26 -0500

    second commit to save current progress most of the styling is done

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var d = new Date();
+var day = d.getDay();
+var monthNum = d.getMonth();
+
+
+console.log(d.getDate());
+
+var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "Arpil", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var currentTime = document.getElementById("currentTime");
+
+currentTime.textContent = weekDay[day] + ", " + month[monthNum] + " " + d.getDate();
+
